04965e1a88e182937af962930f8228170f151bae
sumati jena <sumatijena21997@gmail.com>
2023-12-23 19:31:57 +0530

    Episode 3 Lying the foundation

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom" 
-const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", {}, "this is h1 tag")))
+import ReactDOM from "react-dom/client";
+
+const Title=()=><h1>Namaste React</h1>;
+const Heading=()=>{
+    return <div id="container">
+        <Title/>
+        <h2>Namaste React functional component</h2>
+    </div>
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Heading/>);
