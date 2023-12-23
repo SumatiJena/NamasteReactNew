caab2a672eedb6deb295415ea1d0954df2537929
sumati jena <sumatijena21997@gmail.com>
2023-12-23 18:00:56 +0530

    Epicosde 2nd (igniting App)

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-
-    const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"this is h1 tag")))
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+import React from "react";
+import ReactDOM from "react-dom" 
+const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", {}, "this is h1 tag")))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
